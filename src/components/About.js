9fffd9fd87fb31cb35b98100550fd9ec19eb46f3
Vincent Langat <kimutailangat033@gmail.com>
2022-09-11 14:47:29 +0300

    Set up About page of the App

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function About() {
+  return (
+    <div>
+      <p> This is React Front end App implementing Ruby Sinatra APi.
+        The Pool Management system enables a user to view a list of houses, delete houses and add houses.
+
+      </p>
+
+    </div>
+  )
+}
+
+export default About
