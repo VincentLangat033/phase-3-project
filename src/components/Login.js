36b32bb5a1b429f44a53ad54c6faa78bca007fc0
Vincent Langat <kimutailangat033@gmail.com>
2022-09-13 11:51:28 +0300

    format Login.js complete and whitespaces cleared

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,67 +1,62 @@
-
-
 import React, { useState } from "react";
 import Register from "./Register";
-import "./css/login.css"
+import "./css/login.css";
 
-function Login({ setuseremail, setuserpassword, handlesubmit,setLogged  }) {
-const[signup,setsignup]=useState(false)
-  return (
- 
-    signup?<Register setLogged={setLogged}/>:
-
-    <div className='login'>
-     <div className='loginWrapper'>
-       <div className='loginLeft'>
-          <h3 className='loginLogo'> Pool_House</h3>
-         <span className='loginDesc'> Log in to begin</span>        </div>
-      <div className='loginRight'>
-         <div className='loginBox'>
-        
-    <>
-      <form onSubmit={handlesubmit}>
-        <h1>Please Log In</h1>
-        
-        <input 
-          onChange={(e) => {
-            setuseremail(e.target.value);
-          }}
-          className="loginInput"
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          required
-        />
-        <input
-          onChange={(e) => {
-            setuserpassword(e.target.value);
-          }}
-          className="loginInput"
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          required
-        /> <br></br>
-        <button className="loginRegisterButton" >Login</button>
-      </form>
-      <h6>Don't have an Account?</h6>
-      <button className="loginButtton" onClick={()=>{setsignup(true)}}>Register</button>
-  
-    </>
-    </div>
+function Login({ setuseremail, setuserpassword, handlesubmit, setLogged }) {
+  const [signup, setsignup] = useState(false);
+  return signup ? (
+    <Register setLogged={setLogged} />
+  ) : (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo"> Pool_House</h3>
+          <span className="loginDesc"> Log in to begin</span>{" "}
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <>
+              <form onSubmit={handlesubmit}>
+                <h1>Please Log In</h1>
+                <input
+                  onChange={(e) => {
+                    setuseremail(e.target.value);
+                  }}
+                  className="loginInput"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  required
+                />
+                <input
+                  onChange={(e) => {
+                    setuserpassword(e.target.value);
+                  }}
+                  className="loginInput"
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  required
+                />{" "}
+                <br></br>
+                <button className="loginRegisterButton">Login</button>
+              </form>
+              <h6>Don't have an Account?</h6>
+              <button
+                className="loginButtton"
+                onClick={() => {
+                  setsignup(true);
+                }}
+              >
+                Register
+              </button>
+            </>
+          </div>
+        </div>
       </div>
-     </div>
     </div>
   );
 }
 export default Login;
-
-
-
-
-
-
-
-
